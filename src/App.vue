25d99979955e0afcda8a0f7d356a0a10015226a0
Stanislav <stanislav.nechaev@gmail.com>
2023-09-28 10:34:46 +0300

    add container

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,53 @@
 <script setup>
-
+import MyButton from "@/components/MyButton.vue";
+import { EnvelopeIcon } from "@heroicons/vue/24/solid";
 </script>
 
 <template>
-
+  <div class="container bg-slate-100 flex flex-col gap-8 mx-auto p-10">
+    <div class="flex gap-5">
+      <my-button intent="primary" :left-icon="EnvelopeIcon">Primary</my-button>
+      <my-button intent="secondary" :left-icon="EnvelopeIcon"
+        >Secondary</my-button
+      >
+      <my-button intent="danger" :left-icon="EnvelopeIcon">Danger</my-button>
+      <my-button intent="text" :left-icon="EnvelopeIcon">Text</my-button>
+    </div>
+    <div class="flex gap-5">
+      <my-button disabled intent="primary" :left-icon="EnvelopeIcon"
+        >Primary</my-button
+      >
+      <my-button disabled intent="secondary" :left-icon="EnvelopeIcon"
+        >Secondary</my-button
+      >
+      <my-button disabled intent="danger" :left-icon="EnvelopeIcon"
+        >Danger</my-button
+      >
+      <my-button disabled intent="text" :left-icon="EnvelopeIcon"
+        >Text</my-button
+      >
+    </div>
+    <div class="flex gap-5">
+      <my-button intent="primary" :right-icon="EnvelopeIcon">Primary</my-button>
+      <my-button intent="secondary" :right-icon="EnvelopeIcon"
+        >Secondary</my-button
+      >
+      <my-button intent="danger" :right-icon="EnvelopeIcon">Danger</my-button>
+      <my-button intent="text" :right-icon="EnvelopeIcon">Text</my-button>
+    </div>
+    <div class="flex gap-5">
+      <my-button intent="primary" loading>Primary</my-button>
+      <my-button intent="secondary" loading>Secondary</my-button>
+      <my-button intent="danger" loading>Danger</my-button>
+      <my-button intent="text" loading>Text</my-button>
+    </div>
+    <div class="flex gap-5">
+      <my-button intent="primary">Primary</my-button>
+      <my-button intent="secondary">Secondary</my-button>
+      <my-button intent="danger">Danger</my-button>
+      <my-button intent="text">Text</my-button>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -1,35 +1,24 @@
 <template>
-  <component
-      :is="props.as"
-      :disabled="disabled"
-      :class="buttonClass">
-    <my-loader v-if="loading"/>
+  <component :is="as" :disabled="disabled" :class="buttonClass">
+    <my-loader v-if="loading" />
 
     <component
-        :is="leftIcon"
-        :class="[
-            'w-5 h-5 mr-2',
-            loading && 'invisible'
-            ]"
-
+      :is="leftIcon"
+      :class="['w-5 h-5 mr-2', loading && 'invisible']"
     />
-    <span :class="[
-            loading && 'invisible'
-            ]">
-      <slot/>
+    <span :class="[loading && 'invisible']">
+      <slot />
     </span>
     <component
-        :is="rightIcon"
-        :class="[
-            'w-5 h-5 ml-2',
-            loading && 'invisible'
-            ]"/>
+      :is="rightIcon"
+      :class="['w-5 h-5 ml-2', loading && 'invisible']"
+    />
   </component>
 </template>
 
 <script setup>
-import {computed, reactive, ref} from "vue";
-import {cva} from "class-variance-authority";
+import { computed, reactive, ref } from "vue";
+import { cva } from "class-variance-authority";
 import MyLoader from "@/components/UI/fields/MyLoader.vue";
 
 const props = defineProps({
@@ -39,32 +28,35 @@ const props = defineProps({
   disabled: Boolean,
   as: {
     type: [String, Object],
-    default: 'button',
+    default: "button",
   },
   intent: {
     type: String,
-    validator: (val) => ["primary", "secondary", "danger", "text"].includes(val),
-    default: "secondary"
+    validator: (val) =>
+      ["primary", "secondary", "danger", "text"].includes(val),
+    default: "secondary",
   },
 });
 
 const buttonClass = computed(() => {
-  return cva("inline-flex justify-center items-center text-sm rounded min-h-[32px] px-3 py-0.5 font-semibold ", {
-    variants: {
-      intent: {
-        primary: "bg-black text-white hover:bg-sky-700 ",
-        secondary: "bg-black/5 hover:bg-black/10 text-gray-600",
-        danger: "bg-pink-500 text-white hover:bg-pink-300",
-        text: "text-gray-700 hover:bg-black/5"
+  return cva(
+    "inline-flex justify-center items-center text-sm rounded min-h-[32px] px-3 py-0.5 font-semibold ",
+    {
+      variants: {
+        intent: {
+          primary: "bg-black text-white hover:bg-sky-700 ",
+          secondary: "bg-black/5 hover:bg-black/10 text-gray-600",
+          danger: "bg-pink-500 text-white hover:bg-pink-300",
+          text: "text-gray-700 hover:bg-black/5",
+        },
+        disabled: {
+          true: "!bg-gray-100 !text-gray-400 cursor-not-allowed",
+        },
       },
-      disabled: {
-        true: "!bg-gray-100 !text-gray-400 cursor-not-allowed"
-      }
-    }
-  })({
+    },
+  )({
     intent: props.intent,
     disabled: props.disabled,
-  })
-})
-
+  });
+});
 </script>
