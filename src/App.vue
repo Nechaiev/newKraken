65f3fc4f1f062d3f1baa5638f91f95705cac3b79
Stanislav <stanislav.nechaev@gmail.com>
2023-10-03 01:07:33 +0300

    add components using props

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
+import LoginForm from "@/components/LoginForm.vue";
+import {ref} from "vue";
 
+const input = ref('')
 </script>
 
 <template>
-
+  <div class="container ">
+    <login-form></login-form>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
---
+++ src/components/CustomInput.vue
@@ -0,0 +1,31 @@
+<script setup>
+import WrapperCustomInput from "@/components/WrapperCustomInput.vue";
+
+defineEmits(
+    ['update:modelValue', ]
+)
+
+defineProps({
+  placeholder: String,
+  label: String,
+  error: String,
+  helperText: String,
+  modelValue: String,
+  type:  {
+    type: String,
+    default: "text"
+  },
+  name: String,
+})
+</script>
+
+<template>
+  <wrapper-custom-input :helperText="helperText" :error="error" :label="label">
+    <input :type="type"
+           :placeholder="placeholder"
+           class="peer block min-h-[auto] w-full rounded ring-1 bg-transparent px-3 py-[0.32rem]"
+           @input="$emit('update:modelValue', $event.target.value)"
+           :value="modelValue"
+    >
+  </wrapper-custom-input>
+</template>
