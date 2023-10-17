7269ec5dd543e8c4d8da32d13086afcfe4244c23
Stanislav <stanislav.nechaev@gmail.com>
2023-10-18 00:18:17 +0300

    Update DynamicComponents.vue

--- src/components/vee-validate-form/DynamicComponents.vue
+++ src/components/vee-validate-form/DynamicComponents.vue
@@ -1,13 +1,16 @@
 <template>
-  <div>
-    <button
-      v-for="(component, label) in tabs"
-      :key="label"
-      :class="['tab-button', { active: currentTab === label }]"
-      @click="currentTab = label"
-    >
-      {{ label }}
-    </button>
+  <div >
+    <div class="flex justify-center gap-4 mb-3">
+      <button
+        class="py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-sans rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+        v-for="(component, label) in tabs"
+        :key="label"
+        :class="['tab-button', { active: currentTab === label }]"
+        @click="currentTab = label"
+      >
+        {{ label }}
+      </button>
+    </div>
     <component :is="tabs[currentTab]" class="tab"></component>
   </div>
 </template>
