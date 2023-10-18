7228e1983a875552b2a4c961a011df3b26e8e5ff
Stanislav <stanislav.nechaev@gmail.com>
2023-10-19 00:33:47 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
     <!--    <vee-validate />-->
     <!--      <roles-form/>-->
     <!--    <dynamic-components />-->
-    <roles-form-new />
+    <roles-form-fore-modal />
+    <!--    <roles-form-new />-->
   </div>
 </template>
 
@@ -17,6 +18,7 @@ import VeeValidate from "@/components/provide-inject/VeeValidate.vue";
 import RolesForm from "@/components/vee-validate-form/RolesForm.vue";
 import DynamicComponents from "@/components/vee-validate-form/DynamicComponents.vue";
 import RolesFormNew from "@/components/vee-validate-form/RolesFormNew.vue";
+import RolesFormForeModal from "@/components/vee-validate-form/RolesFormForeModal.vue";
 
 const input = ref("");
 </script>
