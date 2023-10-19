e73f4675fb1be1a3e5d7e0e5c9568c115572c274
Stanislav <stanislav.nechaev@gmail.com>
2023-10-19 10:26:20 +0300

    Update CustomModal.vue

--- src/components/CustomModal.vue
+++ src/components/CustomModal.vue
@@ -1,47 +1,37 @@
 <template>
-  <div class="flex justify-center items-center">
+  <div
+    v-if="openModal"
+    class="fixed z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded p-4 shadow-lg"
+  >
+    <p class="text-xl mb-4">confirm sending data</p>
     <button
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer m-6"
-      @click="openModal = true"
+      @click="confirmForm"
+      class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded cursor-pointer w-full"
     >
-      OPEN A MODAL WINDOW
+      CONFIRM
     </button>
-    <Teleport to="body">
-      <div
-        v-if="openModal"
-        class="fixed z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded p-4 shadow-lg"
-      >
-        <p class="text-xl mb-4">confirm sending data</p>
-        <button
-          @click="confirmForm"
-          class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded cursor-pointer w-full"
-        >
-          CONFIRM
-        </button>
-        <button
-          @click="openModal = false"
-          class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded cursor-pointer w-full mt-4"
-        >
-          REJECT
-        </button>
-      </div>
-    </Teleport>
+    <button
+      @click="onCloseModal"
+      class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded cursor-pointer w-full mt-4"
+    >
+      REJECT
+    </button>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+const props = defineProps({
+    openModal: Boolean
+})
 
-const openModal = ref(false);
+const emit = defineEmits(["confirm", "update:openModal"]);
 
-const values = ref(null)
+const onCloseModal = () => {
+    emit('update:openModal', false)
+}
 
-// const onSubmit = (data) => {
-//     console.log("Form submitted with data:", data);
-// };
 const confirmForm = () => {
-    onSubmit(values.value);
-    openModal.value = false;
+  emit("confirm");
+    onCloseModal()
 };
-
 </script>
