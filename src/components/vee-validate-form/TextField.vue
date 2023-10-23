27e7ec707c99a6fa4a1979d5a23ba53ed42b5441
Stanislav <stanislav.nechaev@gmail.com>
2023-10-23 20:32:52 +0300

    Update TextField.vue

--- src/components/vee-validate-form/TextField.vue
+++ src/components/vee-validate-form/TextField.vue
@@ -7,17 +7,19 @@
       :class="fieldClasses"
       v-model="innerValue"
     />
+    <!--      v-model="innerValue"  it was in the input on tape 10-->
   </field-wrapper>
 </template>
 
 <script setup>
 import FieldWrapper from "@/components/vee-validate-form/FieldWrapper.vue";
 import { useField } from "vee-validate";
+import { computed } from "vue";
 
 const props = defineProps({
   name: {
     type: String,
-    required: true,
+    default: "",
   },
   type: {
     type: String,
@@ -25,11 +27,38 @@ const props = defineProps({
   },
   label: String,
   placeholder: String,
-  mask: String,
+  modelValue: String,
+  validationSchema: null,
+  syncVModel: Boolean,
 });
+defineEmits(["update:modelValue"]);
 
-const { value: innerValue, errors } = useField(() => props.name);
+const { value: innerValue, errors } = useField(
+  () => props.name,
+  props.validationSchema,
+  {
+    syncVModel: props.syncVModel,
+  },
+);
+
+// const innerValue = computed({
+//   get() {
+//     return props.name ? value.value : props.modelValue;
+//   },
+//   set(newVal) {
+//     if (props.name) {
+//       value.value = newVal;
+//       return;
+//     }
+//     emit("update:modelValue", newVal);
+//   },
+// });
 
 const fieldClasses =
   "peer block min-h-[auto] w-full rounded ring-1 bg-transparent px-3 py-[0.32rem] leading-[1.6] placeholder-black::placeholder outline-none transition-all duration-200 ease-linear motion-reduce:transition-none";
 </script>
+
+<!--
+1. прийняти пропс.
+2. Зробити двустороню прив'язку, через useForm, та modelValue
+-->
